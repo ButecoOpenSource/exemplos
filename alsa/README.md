ee0021c23cb2e6e8c2f8f88c8a5d19ce0e33bf24
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2014-10-08 12:49:25 -0300

    Update.

--- alsa/README.md
+++ alsa/README.md
@@ -1,4 +1,4 @@
-Exemplos do [Artigo](http://blog.butecopensource.com/2014/09/24/introducao-ao-alsa-e-sua-api-em-c-e-python/).
+Exemplos do artigo [Introdução ao ALSA e sua API em C e Python](http://blog.butecopensource.com/2014/09/24/introducao-ao-alsa-e-sua-api-em-c-e-python/).
 
 #### Python
 
--- alsa/volume.c
+++ alsa/volume.c
@@ -3,39 +3,39 @@
 
 int main(int argc, char* argv[])
 {
-	long vol_min = 0, vol_max = 0, vol = 0;
-	snd_mixer_t *handle;
-	snd_mixer_elem_t *elem;
-	snd_mixer_selem_id_t *sid;
-	snd_mixer_selem_channel_id_t channel = SND_MIXER_SCHN_FRONT_LEFT;
+    long vol_min = 0, vol_max = 0, vol = 0;
+    snd_mixer_t *handle;
+    snd_mixer_elem_t *elem;
+    snd_mixer_selem_id_t *sid;
+    snd_mixer_selem_channel_id_t channel = SND_MIXER_SCHN_FRONT_LEFT;
 
-	snd_mixer_open(&handle, 0);
-	snd_mixer_attach(handle, "default");
-	snd_mixer_selem_register(handle, NULL, NULL);
-	snd_mixer_load(handle);
+    snd_mixer_open(&handle, 0);
+    snd_mixer_attach(handle, "default");
+    snd_mixer_selem_register(handle, NULL, NULL);
+    snd_mixer_load(handle);
 
-	snd_mixer_selem_id_alloca(&sid);
-	snd_mixer_selem_id_set_index(sid, 0);
+    snd_mixer_selem_id_alloca(&sid);
+    snd_mixer_selem_id_set_index(sid, 0);
 
-	// pegar volume minimo e m·ximo
-	// Master aqui se refere ao volume de saida
-	// geralmente esse È o nome da configuraÁ„o de volume da m·quina
-	snd_mixer_selem_id_set_name(sid, "Master");
-	elem = snd_mixer_find_selem(handle, sid);
+    // pegar volume minimo e m√°ximo
+    // Master aqui se refere ao volume de saida
+    // geralmente esse √© o nome da configura√ß√£o de volume da m√°quina
+    snd_mixer_selem_id_set_name(sid, "Master");
+    elem = snd_mixer_find_selem(handle, sid);
 
-	snd_mixer_selem_get_playback_volume_range(elem, &vol_min, &vol_max);
+    snd_mixer_selem_get_playback_volume_range(elem, &vol_min, &vol_max);
 
-	printf("Volume mÌnimo: %ld e m·ximo: %ld\n", vol_min, vol_max);
+    printf("Volume m√≠nimo: %ld e m√°ximo: %ld\n", vol_min, vol_max);
 
-	if (argc == 1) {
-		fprintf(stderr, "Voce precisa passar um valor %ld e %ld como volume\n", vol_min, vol_max);
-		return 1;
-	}
+    if (argc == 1) {
+        fprintf(stderr, "Voce precisa passar um valor %ld e %ld como volume\n", vol_min, vol_max);
+        return 1;
+    }
 
-	snd_mixer_selem_set_playback_volume_all(elem, atol(argv[1]));
+    snd_mixer_selem_set_playback_volume_all(elem, atol(argv[1]));
 
-	snd_mixer_selem_get_playback_volume(elem, channel, &vol);
-	printf("Volume atual: %ld\n", vol);
+    snd_mixer_selem_get_playback_volume(elem, channel, &vol);
+    printf("Volume atual: %ld\n", vol);
 
-	return 0;
+    return 0;
 }
