332ca29953cd3e1784e06048338f6e0c1874abfc
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2015-01-31 13:39:56 -0200

    Colocar mais comentários no código

--- pipe_fifo/pipe.c
+++ pipe_fifo/pipe.c
@@ -15,14 +15,19 @@ int main()
 		return EXIT_FAILURE;
 	}
 
+	// at this point, a new process will be created and will execute the same as father
+	// the only thing that can jail each process is to test the return of fork system call.
+	// The fork syscall returns 0 in the new process, while return the pid of parent in the case
+	// of the parent process
 	switch(fork()) {
 	case -1:
 		perror("fork");
 		return EXIT_FAILURE;
-	case 0: // child
+	case 0: // child process
 		if (close(fds[1]) == -1) // closes the write fd as it will not be used
 			perror("Child close");
 
+		// child process will wait to read message of parent process
 		if (read(fds[0], buf, sizeof(buf)) == -1) {
 			perror("Child read");
 			return EXIT_FAILURE;
@@ -35,6 +40,7 @@ int main()
 		if (close(fds[0]) == -1) // closes the read fd as it will not be used
 			perror("Child close");
 
+		// parent process writea message to child process
 		strncpy(buf, "Hello from parent! Have a long life!", sizeof(buf));
 		if (write(fds[1], buf, sizeof(buf)) == -1) {
 			perror("Parent write");
