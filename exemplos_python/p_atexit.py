7f146ec5cd98960f28b7b15d754cd33fc2f9b9c7
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2015-02-03 22:24:04 -0200

    Adicionada chamada de decorator

--- exemplos_python/p_atexit.py
+++ exemplos_python/p_atexit.py
@@ -7,6 +7,10 @@ def meChama():
 def meChama2():
 	print 'Serei chamado primeiro!'
 
+@atexit.register
+def meChama3():
+	print 'meChama3 sendo chamado!'
+
 # registrar a chamada da funcao no termino do script
 atexit.register(meChama)
 atexit.register(meChama2)
