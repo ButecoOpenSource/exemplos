7b44f17d8f08fedb1c400768ef36758753eb253d
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2015-02-03 22:21:14 -0200

    Adicionad=a mais uma chamada de atexit no python

--- exemplos_python/p_atexit.py
+++ exemplos_python/p_atexit.py
@@ -4,9 +4,13 @@ import atexit
 def meChama():
 	print 'Funcao chamada no encerramento do script'
 
+def meChama2():
+	print 'Serei chamado primeiro!'
+
 # registrar a chamada da funcao no termino do script
 atexit.register(meChama)
+atexit.register(meChama2)
 
-print 'A funcao meChama ira ser chamada no encerramento do script'
+print 'As funcoes serao chamadas no encerramento do script'
 
 time.sleep(3)
