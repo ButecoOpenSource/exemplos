91cd788bfe2221fd15e7e33414488106bbce5058
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2015-03-25 23:25:34 -0300

    c_python: Adicionado código C++ (std::string) e alterado gcc para g++. Além disso, adicionado exemplo para duplicar string

--- exemplos_python/python_c_integration/modulotestemodule.c
+++ exemplos_python/python_c_integration/modulotestemodule.c
@@ -1,19 +1,34 @@
 #include <Python.h>
 
+#include <iostream>
+
 static PyObject* moduloTesteError;
 
-static PyObject *moduloteste_soma(PyObject *self, PyObject *args)
+static PyObject* moduloteste_soma(PyObject *self, PyObject *args)
 {
 	int valorA, valorB;
 	if (!PyArg_ParseTuple(args, "ii", &valorA, &valorB)) {
-		PyErr_SetString(moduloTesteError, "Parametro inválido!");
+		PyErr_SetString(moduloTesteError, "Parametro inválido! Esperados dois inteiros!");
 		return NULL;
 	}
 	return Py_BuildValue("i", valorA + valorB);
 }
 
+static PyObject* moduloteste_duplicastring(PyObject *self, PyObject *args)
+{
+	char *str;
+	if (!PyArg_ParseTuple(args, "s", &str)) {
+		PyErr_SetString(moduloTesteError, "Parametro inválido! Esperada uma string!");
+		return NULL;
+	}
+	std::string str2(str);
+	str2.append(str2);
+	return Py_BuildValue("s", str2.c_str());
+}
+
 static PyMethodDef MetodosModuloTest[] = {
 	{"soma", moduloteste_soma, METH_VARARGS, "Recebe dois inteiro e devolve a soma de ambos"},
+	{"duplicastring", moduloteste_duplicastring, METH_VARARGS, "Recebe uma string e retorna a mesma duplicada"},
 	{NULL, NULL, 0, NULL}
 };
 
@@ -23,6 +38,8 @@ initmoduloteste(void)
 	PyObject* init = Py_InitModule("moduloteste", MetodosModuloTest);
 	if (!init)
 		return;
-	moduloTesteError = PyErr_NewException("moduloteste.erro", NULL, NULL);
+
+	std::string erro = "moduloteste.erro";
+	moduloTesteError = PyErr_NewException(const_cast<char *>(erro.c_str()), NULL, NULL);
 	PyModule_AddObject(init, "erro", moduloTesteError);
 }
--- exemplos_python/python_c_integration/testacalculadora.py
+++
@@ -1,9 +0,0 @@
-import moduloteste
-
-param1 = 1
-param2 = 3
-
-try:
-	print 'Valor de ' + str(param1) + ' + ' + str(param2) + ': ' + str(moduloteste.soma(param1, param2))
-except moduloteste.erro:
-	print 'erro na lib'
