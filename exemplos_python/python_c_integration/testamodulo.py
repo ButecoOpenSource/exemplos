57b5c8a3e90f3593566b6a779658cb31c83885c0
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2015-05-12 00:27:53 -0300

    Adicionado exemplo de chamada ioctl para abrir o drive de CD-ROM

--- exemplos_python/python_c_integration/testamodulo.py
+++ exemplos_python/python_c_integration/testamodulo.py
@@ -1,18 +1,27 @@
-import moduloteste
+from memory_profiler import profile
 
-print dir(moduloteste)
+a = ['marcos paulo de souza', 'marcos paulo de souza', 'marcos paulo de souza', 'marcos paulo de souza', 'marcos paulo de souza', 'marcos paulo de souza']
 
-param1 = 1
-param2 = 3
+@profile
+def a():
+	global a
+	import moduloteste
+	param1 = 1
+	param2 = 3
 
-param3 = 'teste'
+	param3 = 'teste'
 
-try:
-	print 'Valor de ' + str(param1) + ' + ' + str(param2) + ': ' + str(moduloteste.soma(param1, param2))
-except moduloteste.erro as e:
-	print 'erro na lib: ' + str(e)
+	try:
+		print 'Valor de ' + str(param1) + ' + ' + str(param2) + ': ' + str(moduloteste.soma(param1, param2))
+	except moduloteste.erro as e:
+		print 'erro na lib: ' + str(e)
 
-try:
-	print 'String ' + param3 + ' duplicada' + ': ' + moduloteste.duplicastring(param3)
-except moduloteste.erro as e:
-	print 'erro na lib: ' + str(e)
+	try:
+		print 'String ' + param3 + ' duplicada' + ': ' + moduloteste.duplicastring(param3)
+	except moduloteste.erro as e:
+		print 'erro na lib: ' + str(e)
+
+	print a
+	a.clear
+
+a()
