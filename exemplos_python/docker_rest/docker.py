e4c708a4d44ee4ebf2008f4dc240fcd8de224cee
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-10-12 13:06:49 -0300

    Better code.
    PEP8 and Python 3 compatible.

--- exemplos_python/docker_rest/docker.py
+++ exemplos_python/docker_rest/docker.py
@@ -1,121 +1,139 @@
-import json
+import requests
 import sys
 
-import requests
-
-ipaddr = ''
 
 def menu():
-	print('\nMenu\n\n' +
-		'1: Show containers\n' + 
-		'2: Create container\n' +
-		'3: Delete container\n' +
-		'4: Delete all containers\n' +
-		'5: Show images\n' +
-		'6: Pull Image\n' +
-		'7: Delete image\n' +
-		'8: Delete all images\n' +
-		'9: Quit')
+    print('''
+Menu\n
+1: Show containers
+2: Create container
+3: Delete container
+4: Delete all containers
+5: Show images
+6: Pull Image
+7: Delete image
+8: Delete all images
+9: Quit
+''')
 
-def verifyError(data, code, show_error = True):
-	if str(data) == '<Response [' + code + ']>':
-		if show_error:
-			print('Command OK')
-		return True
-	if show_error:
-		print('ERROR: ' + code)
-	return False
 
-def getContainers(show = True):
-	ret = requests.get(ipaddr + '/containers/json?all=1')
-	if verifyError(ret, '200'):
-		if show:
-			for i in ret.json():
-				print i
-		else:
-			return ret.json()
+def verify_error(response, code, show_error=True):
+    if response.status_code == code:
+        if show_error:
+            print('Command OK')
+        return True
 
-def deleteContainer(uid = None):
-	if uid:
-		cont = uid
-	else:
-		cont = raw_input("Insert the container UID: ")
-	ret = requests.delete(ipaddr + '/containers/' + cont)
-	return verifyError(ret, '204')
+    if show_error:
+        print('ERROR: %d' % response.status_code)
 
-def createContainer():
-	image = raw_input('Insert image:tag name: ')
-	cont_name = raw_input('Insert container name: ')
+    return False
 
-	payload = {"Image":image, 'Entrypoint':'bash'}
-	ret = requests.post(ipaddr + '/containers/create?name=' + cont_name, json=payload)
-	verifyError(ret, '201')
 
-def deleteAllContainers():
-	err = []
-	lst = getContainers(False)
-	for i in lst:
-		deleteContainer(i['Id'])
+def get_containers(ipaddr, show=True):
+    ret = requests.get('%s/containers/json?all=1' % ipaddr)
 
-def getImages(show = True):
-	ret = requests.get(ipaddr + '/images/json?all=0')
-	if verifyError(ret, '200'):
-		if show:
-			for i in ret.json():
-				print i
-		else:
-			return ret.json()
+    if verify_error(ret, 200):
+        if show:
+            for i in ret.json():
+                print(i)
+        else:
+            return ret.json()
 
-def createImage():
-	name = raw_input('Insert the image name: ')
-	tag = raw_input('Insert tag name: ')
-	print('Pulling image...')
-	ret = requests.post(ipaddr + '/images/create?fromImage=' + name + '&tag=' + tag)
-	verifyError(ret, '200')
 
-def deleteImage(iname = None):
-	if iname:
-		name = iname
-	else:
-		name = raw_input('Insert the image name to be removed: ')
+def delete_container(ipaddr, uid=None):
+    if uid:
+        cont = uid
+    else:
+        cont = input('Insert the container UID: ')
 
-	ret = requests.delete(ipaddr + '/images/' + name)
-	verifyError(ret, '200')
+    ret = requests.delete('%s/containers/%s' % (ipaddr, cont))
+    return verify_error(ret, 204)
 
-def deleteAllImages():
-	err = []
-	lst = getImages(False)
-	for i in lst:
-		deleteImage(i['RepoTags'][0])
 
-if len(sys.argv) != 2:
-	print('Usage: python docker.py <ip of the docker daemon>')
-	sys.exit(1)
+def create_container(ipaddr):
+    image = input('Insert image:tag name: ')
+    cont_name = input('Insert container name: ')
 
-# default port used is 2375
-ipaddr = 'http://' + sys.argv[1] + ':2375'
+    payload = {'Image': image, 'Entrypoint': 'bash'}
+    ret = requests.post('%s/containers/create?name=%s' % (ipaddr, cont_name), json=payload)
+    verify_error(ret, 201)
 
-while True:
-	menu()
-	
-	opt = raw_input('Choise: ')
-	if opt == '1':
-		getContainers()
-	elif opt == '2':
-		createContainer()
-	elif opt == '3':
-		deleteContainer()
-	elif opt == '4':
-		deleteAllContainers()
-	elif opt == '5':
-		getImages()
-	elif opt == '6':
-		createImage()
-	elif opt == '7':
-		deleteImage()
-	elif opt == '8':
-		deleteAllImages()
-	elif opt == '9':
-		break
 
-print('Bye!')
+def delete_all_containers(ipaddr):
+    lst = get_containers(False)
+
+    for i in lst:
+        delete_container(i['Id'])
+
+
+def get_images(ipaddr, show=True):
+    ret = requests.get('%s/images/json?all=0' % ipaddr)
+
+    if verify_error(ret, 200):
+        if show:
+            for i in ret.json():
+                print(i)
+        else:
+            return ret.json()
+
+
+def create_image(ipaddr):
+    name = input('Insert the image name: ')
+    tag = input('Insert tag name: ')
+
+    print('Pulling image...')
+
+    ret = requests.post('%s/images/create?fromImage=%s&tag=%s' % (ipaddr, name, tag))
+    verify_error(ret, 200)
+
+
+def delete_image(ipaddr, iname=None):
+    if iname:
+        name = iname
+    else:
+        name = input('Insert the image name to be removed: ')
+
+    ret = requests.delete('%s/images/%s' % (ipaddr, name))
+    verify_error(ret, 200)
+
+
+def delete_all_images(ipaddr):
+    lst = get_images(False)
+
+    for i in lst:
+        delete_image(i['RepoTags'][0])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python docker.py <ip of the docker daemon>')
+        sys.exit(1)
+
+    # default port used is 2375
+    ipaddr = 'http://%s:2375' % (sys.argv[1])
+
+    while True:
+        menu()
+
+        opt = input('Choice: ')
+
+        if opt == 1:
+            get_containers(ipaddr)
+        elif opt == 2:
+            create_container(ipaddr)
+        elif opt == 3:
+            delete_container(ipaddr)
+        elif opt == 4:
+            delete_all_containers(ipaddr)
+        elif opt == 5:
+            get_images(ipaddr)
+        elif opt == 6:
+            create_image(ipaddr)
+        elif opt == 7:
+            delete_image(ipaddr)
+        elif opt == 8:
+            delete_all_images(ipaddr)
+        elif opt == 9:
+            break
+
+    print('Bye!')
