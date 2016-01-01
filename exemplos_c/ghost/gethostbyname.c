ba34c10fadcc743e389e275e4c5ba9cb98af1e62
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2016-01-01 13:26:48 -0200

    Utilizar gtethostbyname2 pois gethostbyname não traz endereços IPv6

--- exemplos_c/ghost/gethostbyname.c
+++ exemplos_c/ghost/gethostbyname.c
@@ -6,30 +6,49 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char **argv)
+void print_ips(char *hostname, int iptype)
 {
-	if (argc < 2) {
-		printf("Usage: gethostbyname <address or hostname>\n");
-		return EXIT_FAILURE;
-	}
-
-	struct hostent *ht = gethostbyname(argv[1]);
+	struct hostent *ht = gethostbyname2(hostname, iptype);
 	if (!ht) {
 		printf("Error: %s\n", hstrerror(h_errno));
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	printf("Host: %s\n", ht->h_name);
 
 	int i;
-	if (ht->h_addrtype == AF_INET) {
-		for (i = 0; ht->h_addr_list[i] != NULL; i++) {
+	char addr[INET6_ADDRSTRLEN];
+	for (i = 0; ht->h_addr_list[i] != NULL; i++) {
+		void *ptr = NULL;
+
+		if (ht->h_addrtype == AF_INET) {
 			struct in_addr *tmp = (struct in_addr *)ht->h_addr_list[i];
-			printf("IP: %s\n", inet_ntoa(*tmp));
+			ptr = tmp;
+			printf("IPv4: ");
+		} else if (ht->h_addrtype == AF_INET6) {
+			struct in6_addr *tmp = (struct in6_addr *)ht->h_addr_list[i];
+			ptr = tmp;
+			printf("IPv6: ");
 		}
-	} else { // AF_INET6
-		//struct in6_addr *tmp = (struct in6_addr *)ht->h_addr_list[i];
-		printf("IPv6 TODO\n");
+
+		if (!inet_ntop(ht->h_addrtype, ptr, addr, INET6_ADDRSTRLEN)) {
+			perror("inet_ntop");
+			exit(EXIT_FAILURE);
+		}
+
+		printf("%s\n", addr);
 	}
-	return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		printf("Usage: gethostbyname <address or hostname>\n");
+		exit(EXIT_FAILURE);
+	}
+
+	print_ips(argv[1], AF_INET);
+	print_ips(argv[1], AF_INET6);
+
+	exit(EXIT_SUCCESS);
 }
