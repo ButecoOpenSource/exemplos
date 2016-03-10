736002422a615c536053b2fc1193368e27aff930
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2016-03-10 00:10:05 -0300

    Exemplo de utilização de POSIX regex em C

---
+++ exemplos_c/posix_regex/regex.c
@@ -0,0 +1,84 @@
+#include <sys/types.h>
+#include <regex.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void exec_regex(char *str, char *pat, int args)
+{
+	regex_t reg;
+	printf("String '%s', Pattern '%s'\n", str, pat);
+
+	if (regcomp(&reg, pat, args)) {
+		fprintf(stderr, "Could not compiled regex\n");
+		return;
+	}
+
+	int ret, nsub = reg.re_nsub;
+	// if nsub == 0 | > 1, add 1 to show the one string matches
+	if (nsub != 1)
+		nsub++;
+
+	regmatch_t matchptr[nsub];
+
+	if ((ret = regexec(&reg, str, nsub, matchptr, 0))) {
+		if (ret == REG_NOMATCH) {
+			fprintf(stderr, "\tREG_NOMATCH\n");
+		} else {
+			char err[100];
+			regerror(ret, &reg, err, sizeof(err));
+			fprintf(stderr, "\tRegex failed: %s\n", err);
+		}
+		return;
+	} else {
+		int i;
+
+		for (i = 0; i < nsub; i++) {
+			if (matchptr[i].rm_so == -1)
+				break;
+
+			// create a string long enough to store the original str
+			char string[strlen(str) + 1];
+			// copy the the first offset fraction of the original string
+			strcpy(string, str + matchptr[i].rm_so);
+			// end string if the end of offset returned by regexec
+			string[matchptr[i].rm_eo - matchptr[i].rm_so] = 0;
+			printf("\tGroup %d: [%u-%u]: %s\n", i, matchptr[i].rm_so, matchptr[i].rm_eo, string);
+		}
+	}
+
+	regfree(&reg);
+	return;
+}
+
+static void exec_regex_basic(char *str, char *pat)
+{
+	exec_regex(str, pat, 0);
+}
+
+static void exec_regex_extended(char *str, char *pat)
+{
+	exec_regex(str, pat, REG_EXTENDED);
+}
+
+int main()
+{
+	printf("Executing basic Regex\n");
+	exec_regex_basic("buteco123xxxxx", "\\([a-z]*\\)");
+	exec_regex_basic("buteco123xxxxx", "\\([a-z]+\\)\\([0-9]+\\)");
+	exec_regex_basic("buteco123xxxxx", "\\([a-z]*\\)\\([0-9]*\\)");
+	exec_regex_basic("buteco123xxxxx", "\\([[:alpha:]]*\\)");
+
+	printf("\nExecuting extended Regex\n");
+	exec_regex_extended("buteco123xxxxx", "([a-z]*)");
+	exec_regex_extended("buteco123xxxxx", "([a-z]+)([0-9]+)");
+	exec_regex_extended("buteco123xxxxx", "([a-z]*)([0-9]*)");
+	exec_regex_extended("buteco123xxxxx", "([[:alpha:]]*)");
+	exec_regex_extended("buteco123xxxxx", "([^[:space:]]+)");
+	exec_regex_extended("buteco123xxxxx", "([[:digit:]]+)");
+	exec_regex_extended("buteco123xxxxx", "([[:upper:]]+)");
+	exec_regex_extended("buteco123xxxxx", "([[:lower:]])");
+
+	exit(EXIT_SUCCESS);
+}
